d390cb5594d180e8e3c1c6abdacade5f7454a94e
dirkarnez <smalldirkalex@gmail.com>
2025-01-19 21:47:30 +0800

    - wip

--- bookmarklet.js
+++ bookmarklet.js
@@ -20,16 +20,17 @@ function titleCase(str) {
     .split(/\s+/)
     .map(a => a.charAt(0).toUpperCase() + a.slice(1))
     .join(String.fromCharCode(0x20))
+    .trim()
 }
 
 const TD_SUBJECT_CODE_INDEX = 0;
 const TD_SUBJECT_TITLE_INDEX = 1;
 const TD_SUBJECT_WEEKDAY_INDEX = 7;
 const TD_SUBJECT_START_TIME_INDEX = 8;
-const TD_SUBJECT_END_TIME_INDEX = 8;
-const TD_SUBJECT_VENUE_INDEX = 9;
+const TD_SUBJECT_END_TIME_INDEX = 9;
+const TD_SUBJECT_VENUE_INDEX = 10;
 
-Array
+const output = Array
 .from(document.getElementsByTagName('*'))
 .filter(a => a.textContent == "Subject Group Code" && a.children.length == 0)
 .reduce((prev, current) => {
@@ -44,16 +45,15 @@ Array
                     ...Array
                     .from(currentTBody.children)
                     .reduce((prevLine, currentTR) => {
-                        debugger;
                         return [
                             ...prevLine,
                             {
-                                "subject_code": currentTR.children[TD_SUBJECT_CODE_INDEX].textContent,
+                                "subject_code": currentTR.children[TD_SUBJECT_CODE_INDEX].textContent.trim(),
                                 "subject_title": titleCase(currentTR.children[TD_SUBJECT_TITLE_INDEX].textContent),
-                                "weekday": currentTR.children[TD_SUBJECT_WEEKDAY_INDEX].textContent,
-                                "start_time": currentTR.children[TD_SUBJECT_START_TIME_INDEX].textContent,
-                                "end_time": currentTR.children[TD_SUBJECT_END_TIME_INDEX].textContent,
-                                "venue": currentTR.children[TD_SUBJECT_VENUE_INDEX].textContent
+                                "weekday": currentTR.children[TD_SUBJECT_WEEKDAY_INDEX].textContent.trim(),
+                                "start_time": currentTR.children[TD_SUBJECT_START_TIME_INDEX].textContent.trim(),
+                                "end_time": currentTR.children[TD_SUBJECT_END_TIME_INDEX].textContent.trim(),
+                                "venue": currentTR.children[TD_SUBJECT_VENUE_INDEX].textContent.trim()
                             }
                         ]
                     }, [])
@@ -62,3 +62,6 @@ Array
         )
     ];
 }, []);
+
+console.log(JSON.stringify(output));
+
